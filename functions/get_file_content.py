8720a4a53b8bab92aad779e2403682b3a7ea8e7f
urielbrunomota <ubumo@hotmail.com>
2025-06-24 21:54:16 -0400

    debugged get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,16 +2,17 @@ import os
 
 def get_file_content(working_directory, file_path):
     working_directory = os.path.abspath(working_directory)
-    file_path = os.path.abspath(file_path)
+    target_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if os.path.commonpath([working_directory, file_path]) != working_directory:
+    if os.path.commonpath([working_directory, target_file_path]) != working_directory:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(file_path):
+
+    if not os.path.isfile(target_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
 
     try:
         MAX_CHARS = 10001
-        with open(file_path, "r") as f:
+        with open(target_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
             if len(file_content_string)>10000:
                 file_content_string = file_content_string[:10000] + f"[...File \"{file_path}\" truncated at 10000 characters]"
--- tests.py
+++ tests.py
@@ -1,17 +1,26 @@
 from functions.get_file_content import get_file_content
 
 def run_tests():
-    print("Test 1: get_files_info('calculator', '.')")
-    print(get_files_info("calculator", "."), end="\n\n")
+    #print("Test 1: get_file_content ('calculator', 'lorem.txt')")
+    #print(get_file_content("calculator", "lorem.txt"), end="\n\n")
 
-    print("Test 2: get_files_info('calculator', 'pkg')")
-    print(get_files_info("calculator", "pkg"), end="\n\n")
+    print("Test 1: get_file_content ('calculator', 'main.py')")
+    print(get_file_content("calculator", "main.py"), end="\n\n")
 
-    print("Test 3: get_files_info('calculator', '/bin')")
-    print(get_files_info("calculator", "/bin"), end="\n\n")
+    print("Test 2: get_file_content ('calculator', 'pkg/calculator.py')")
+    print(get_file_content("calculator", "pkg/calculator.py"), end="\n\n")
 
-    print("Test 4: get_files_info('calculator', '../')")
-    print(get_files_info("calculator", "../"), end="\n\n")
+    print("Test 3: get_file_content ('calculator', '/bin/cat')")
+    print(get_file_content("calculator", "/bin/cat"), end="\n\n")
+
+    #print("Test 2: get_files_info('calculator', 'pkg')")
+    #print(get_files_info("calculator", "pkg"), end="\n\n")
+
+    #print("Test 3: get_files_info('calculator', '/bin')")
+    #print(get_files_info("calculator", "/bin"), end="\n\n")
+
+    #print("Test 4: get_files_info('calculator', '../')")
+    #print(get_files_info("calculator", "../"), end="\n\n")
 
 if __name__ == "__main__":
     run_tests()
