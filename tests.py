838ae2d84236adf2d79a71ebd06e34d12080eaed
urielbrunomota <ubumo@hotmail.com>
2025-06-25 20:24:49 -0400

    Debugged write_file.py

--- tests.py
+++ tests.py
@@ -1,17 +1,17 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def run_tests():
     #print("Test 1: get_file_content ('calculator', 'lorem.txt')")
     #print(get_file_content("calculator", "lorem.txt"), end="\n\n")
 
-    print("Test 1: get_file_content ('calculator', 'main.py')")
-    print(get_file_content("calculator", "main.py"), end="\n\n")
+    #print("Test 1: get_file_content ('calculator', 'main.py')")
+    #print(get_file_content("calculator", "main.py"), end="\n\n")
 
-    print("Test 2: get_file_content ('calculator', 'pkg/calculator.py')")
-    print(get_file_content("calculator", "pkg/calculator.py"), end="\n\n")
+    #print("Test 2: get_file_content ('calculator', 'pkg/calculator.py')")
+    #print(get_file_content("calculator", "pkg/calculator.py"), end="\n\n")
 
-    print("Test 3: get_file_content ('calculator', '/bin/cat')")
-    print(get_file_content("calculator", "/bin/cat"), end="\n\n")
+    #print("Test 3: get_file_content ('calculator', '/bin/cat')")
+    #print(get_file_content("calculator", "/bin/cat"), end="\n\n")
 
     #print("Test 2: get_files_info('calculator', 'pkg')")
     #print(get_files_info("calculator", "pkg"), end="\n\n")
@@ -21,6 +21,15 @@ def run_tests():
 
     #print("Test 4: get_files_info('calculator', '../')")
     #print(get_files_info("calculator", "../"), end="\n\n")
+
+    print("Test 1: write_file('calculator', 'lorem.txt', 'wait, this isn't lorem ipsum')")
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+
+    print("Test 2: write_file('calculator', 'pkg/morelorem.txt', 'lorem ipsum dolor sit amet')")
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+
+    print("Test 3: write_file('calculator', '/tmp/temp.txt', 'this should not be allowed')")
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     run_tests()
